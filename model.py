dcda2e309b2662dc7e7c52685c6023dfc20322c6
luhouyang <luhouyang@gmail.com>
2024-05-31 23:41:02 +0800

    confusion

--- model.py
+++ model.py
@@ -536,7 +536,7 @@ class TrainingPlot(tf.keras.callbacks.Callback):
 
 
 def confusion(val_ds):
-    class_values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                          'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
                          'Y', 'Z', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'n', 'q', 'r', 't']
     
@@ -553,8 +553,8 @@ def confusion(val_ds):
     plt.figure(figsize=(20, 20))
     plt.autoscale(tight=True)
     sns.heatmap(confusion_mtx,
-                xticklabels=class_values,
-                yticklabels=class_values,
+                xticklabels=classes,
+                yticklabels=classes,
                 annot=True, fmt='g')
     plt.xlabel('Prediction')
     plt.ylabel('Label')
